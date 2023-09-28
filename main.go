c571ada04c0ddcef153548e8efd675624e7ef87a
Rainfog <muhammadzhafranbahij@gmail.com>
2023-09-28 12:04:59 +0700

    CRUD todolist

--- main.go
+++ main.go
@@ -1,7 +1,178 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint
+	Name      string
+	Email     string
+	Age       int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Todo struct {
+	ID        uint
+	UserID    uint
+	User      User `gorm:"foreignKey:UserID"`
+	Event     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
 
 func main() {
-	fmt.Println("Yahoo!")
+
+	//* Menyalakan DATAABASE
+	dsn := "host=127.0.0.1 user=postgres password= dbname=todolist port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	//* Migrate DATABASE
+	// db.AutoMigrate(&User{}, &Todo{})
+
+	router := gin.Default()
+
+	//* GET ALL USERS
+	router.GET("/users", func(c *gin.Context) {
+
+		var users []User
+
+		db.Debug().Preload("Todo").Order("id desc").Find(&users)
+
+		c.JSON(200, gin.H{
+			"message": http.StatusOK,
+			"data":    users,
+		})
+	})
+
+	//* CREATE USERS
+	router.POST("/users", func(c *gin.Context) {
+
+		age, _ := strconv.Atoi(c.PostForm("age"))
+
+		new_users := User{
+			Name:  c.PostForm("name"),
+			Email: c.PostForm("email"),
+			Age:   age,
+		}
+
+		db.Save(&new_users)
+
+		c.JSON(200, gin.H{
+			"message": http.StatusOK,
+			"data":    new_users,
+		})
+
+	})
+
+	//* UPDATE USERS
+	router.PUT("/users/:id", func(c *gin.Context) {
+
+		id, _ := strconv.Atoi(c.Param("id"))
+		age, _ := strconv.Atoi(c.PostForm("age"))
+
+		user := User{
+			ID:    uint(id),
+			Name:  c.PostForm("name"),
+			Email: c.PostForm("email"),
+			Age:   age,
+		}
+
+		db.Save(&user)
+
+		c.JSON(200, gin.H{
+			"message": http.StatusOK,
+			"data":    user,
+		})
+
+	})
+
+	//* DELETE USERS
+	router.DELETE("/users/:id", func(c *gin.Context) {
+
+		id, _ := strconv.Atoi(c.Param("id"))
+
+		db.Delete(&User{}, uint(id))
+
+		c.JSON(200, gin.H{
+			"message": http.StatusOK,
+		})
+	})
+
+	//* GET TODOLIST in THAT USER
+	router.GET("/users/:userid/todolist", func(c *gin.Context) {
+
+		id, _ := strconv.Atoi(c.Param("userid"))
+
+		var todo []Todo
+
+		db.Debug().Order("id desc").Where("user_id = ?", uint(id)).Find(&todo)
+
+		c.JSON(200, gin.H{
+			"message": http.StatusOK,
+			"data":    todo,
+		})
+	})
+
+	//* POST TODOLIST in THAT USER
+	router.POST("/users/:userid/todolist", func(c *gin.Context) {
+
+		id, _ := strconv.Atoi(c.Param("userid"))
+		event := c.PostForm("event")
+
+		todo := Todo{
+			UserID: uint(id),
+			Event:  event,
+		}
+
+		db.Debug().Save(&todo)
+
+		c.JSON(200, gin.H{
+			"message": http.StatusOK,
+			"data":    todo,
+		})
+	})
+
+	//* POST TODOLIST in THAT USER
+	router.PUT("/users/:userid/todolist/:todoid", func(c *gin.Context) {
+
+		id, _ := strconv.Atoi(c.Param("userid"))
+		todo_id, _ := strconv.Atoi(c.Param("todoid"))
+		event := c.PostForm("event")
+
+		todo := Todo{
+			ID:     uint(todo_id),
+			UserID: uint(id),
+			Event:  event,
+		}
+
+		db.Debug().Save(&todo)
+
+		c.JSON(200, gin.H{
+			"message": http.StatusOK,
+			"data":    todo,
+		})
+	})
+
+	//* DELETE USERS
+	router.DELETE("/users/:id/todolist/:todoid", func(c *gin.Context) {
+
+		todo_id, _ := strconv.Atoi(c.Param("todoid"))
+
+		db.Delete(&Todo{}, uint(todo_id))
+
+		c.JSON(200, gin.H{
+			"message": http.StatusOK,
+		})
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
+
 }
